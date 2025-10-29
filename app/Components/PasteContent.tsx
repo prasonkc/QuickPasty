dd60450bea233213c88a885eff0c05c891594758
Prason Kc <prasonkc07@gmail.com>
2025-10-29 11:50:38 +0545

    demo ui

---
+++ app/Components/PasteContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PasteContent = () => {
+  return (
+    
+    <div>PasteContent</div>
+  )
+}
+
+export default PasteContent
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import Image from "next/image";
+import PasteContent from "./Components/PasteContent";
+import PasteUi from "./Components/PasteUI";
+
 
 export default function Home() {
   return (
   <div>
-      
+      <PasteUi />
   </div>
-
   );
 }
